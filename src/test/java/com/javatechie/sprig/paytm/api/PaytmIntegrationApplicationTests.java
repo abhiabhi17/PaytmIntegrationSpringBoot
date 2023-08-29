ac68ac952ce40433e9652a36206a9f9baf8a1b27
User <pavadi.abhishek@gmail.com>
2023-08-29 11:35:57 -0700

    Paytm integration Initial commit

---
+++ src/test/java/com/javatechie/sprig/paytm/api/PaytmIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javatechie.sprig.paytm.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaytmIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
